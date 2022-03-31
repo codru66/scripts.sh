72117fdd6ac345cb50198b4f3d0725abdb0f6545
codru66 <102724087+codru66@users.noreply.github.com>
2022-03-31 10:06:58 +0200

    Create aptInstall.sh

---
+++ aptInstall.sh
@@ -0,0 +1,23 @@
+#CLEAN/FRESH 
+apt update -y
+apt upgrade -y
+
+#APT
+apt install nodejs -y
+apt install nginx -y
+apt install mariadb-server -y
+apt install php-fpm php-mysql -y
+apt install timeshift -y
+apt install python -y
+apt install fail2ban -y
+apt install preload -y
+apt install bleachbit -y
+apt install pulseaudio -y
+apt install wget -y
+apt install terminator -y
+apt install git -y
+apt install grub-customizer -y
+apt install npm -y
+
+apt autoremove -y
+apt autoclean -y
