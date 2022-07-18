77fff482694818eca4cf3c83482ff57cd82b6d7f
codru66 <102724087+codru66@users.noreply.github.com>
2022-07-18 08:04:47 +0200

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,53 @@
+#CLEAN/FRESH 
+apt update -y
+apt upgrade -y
+
+#APT
+apt install nodejs -y
+apt install nginx -y
+apt install mariadb-server -y
+apt install php-fpm php-mysql -y
+apt install timeshift -y
+apt install fail2ban -y
+apt install preload -y
+apt install bleachbit -y
+apt install pulseaudio -y
+apt install wget -y
+apt install git -y
+apt install dotnet-sdk-6.0 -y
+apt install openssh-server openssh-client -y
+apt install net-tools -y
+apt install vlc -y
+apt install gimp -y
+apt install cmatrix -y
+apt install snapd -y
+
+
+
+apt autoremove -y
+apt autoclean -y
+
+#Flatpak
+flatpak install flathub com.visualstudio.code -y
+flatpak install flathub com.spotify.Client -y
+flatpak install flathub org.signal.Signal -y
+flatpak install flathub org.libreoffice.LibreOffice
+
+#SNAP
+snap install solaar
+
+#DEB
+#https://1password.com/downloads/linux/
+#https://brave.com/linux/
+#https://launchpad.net/ubuntu/+source/transmission
+#https://safing.io/portmaster/
+#https://www.synaptics.com/products/displaylink-graphics/downloads/ubuntu
+
+#DEB install
+dnf install 1password -y
+
+#ADD
+
+
+#APPS to install
+#Solaar
