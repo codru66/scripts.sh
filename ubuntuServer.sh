f701264e646a18ef7fea50d96b1e18579af4a8cb
codru66 <102724087+codru66@users.noreply.github.com>
2022-07-28 07:56:09 +0200

    Update ubuntuServer.sh

--- ubuntuServer.sh
+++ ubuntuServer.sh
@@ -1,1 +1,24 @@
+#CLEAN/FRESH 
+apt update -y
+apt upgrade -y
 
+#APT
+apt install nodejs -y
+apt install nginx -y
+apt install mariadb-server -y
+apt install php-fpm php-mysql -y
+apt install timeshift -y
+apt install fail2ban -y
+apt install preload -y
+apt install bleachbit -y
+apt install wget -y
+apt install git -y
+apt install dotnet-sdk-6.0 -y
+apt install openssh-server openssh-client -y
+apt install net-tools -y
+apt install cmatrix -y
+apt install snapd -y
+
+
+apt autoremove -y
+apt autoclean -y
