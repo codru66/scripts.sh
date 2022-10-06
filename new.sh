8aac2470a8fb44b8b90fdbe347fed7678147efb4
codru66 <102724087+codru66@users.noreply.github.com>
2022-10-06 10:02:09 +0200

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,47 @@
+apt autoclean -y
+apt update -y 
+
+apt install nodejs -y
+apt install timeshift -y
+apt install fail2ban -y
+apt install preload -y
+apt install bleachbit -y
+apt install wget -y
+apt install git -y
+apt install npm -y
+apt install nmap -y
+apt install piper -y
+apt install net-tools
+apt install vlc -y
+apt install gimp -y
+apt install cmatrix -y
+apt install snapd -y
+apt install curl -y
+apt install libnetfilter-queue1 libappindicator3-1 -y
+
+apt autoremove -y
+apt autoclean -y
+apt update -y
+
+#Snap
+snap install spotify -y
+snap install docker -y
+
+#installer
+wget https://updates.safing.io/latest/linux_amd64/packages/portmaster-installer.deb -y
+dpkg -i portmaster-installer.deb -y
+
+#Run docker
+sudo addgroup --system docker
+sudo adduser freddie docker
+newgrp docker
+sudo snap disable docker
+sudo snap enable docker
+
+#portainer
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:2.9.3
+               
